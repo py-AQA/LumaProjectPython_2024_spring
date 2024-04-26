0ecc436a2b2589b518c7bcfacdfb6b71e375ee2b
Mila Arh <rover321@mail.ru>
2024-04-26 12:49:09 +0400

    add test product page

--- pages/locators.py
+++ pages/locators.py
@@ -6,5 +6,16 @@ class SalePageLocators():
     FITNESS_EQUIPMENT_LINK = "//a[text()='Fitness Equipment']"
 
 
+class ProductLocators():
+    RADIANT_TEE_URL = 'https://magento.softwaretestingboard.com/radiant-tee.html'
+    RADIANT_TEE_SIZE = '[option-label="XS"]'
+    RADIANT_TEE_COLOR = '[option-label="Orange"]'
+    RADIANT_TEE_QTY = '#qty'
+    ADD_TO_CART_BUTTON = '#product-addtocart-button'
+
+
+
+
+
 class BaseLocators():
     PAGE_NAME = ".base"
---
+++ pages/product_page.py
@@ -0,0 +1,12 @@
+from selene.support.shared.jquery_style import s, ss
+from locators import ProductLocators as PL
+from pages.base_page import BasePage
+
+
+class ProductPage(BasePage):
+    def add_to_cart(self):
+        s(PL.RADIANT_TEE_SIZE).click()
+        s(PL.RADIANT_TEE_COLOR).click()
+        s(PL.RADIANT_TEE_QTY).type('2')
+        s(PL.ADD_TO_CART_BUTTON).click()
+
