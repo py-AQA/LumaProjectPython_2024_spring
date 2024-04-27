76672a2c0d237b16f3e2d69b199ebeff2e7f1346
Mila Arh <rover321@mail.ru>
2024-04-28 01:11:57 +0400

    add test checkout sleep (#12)

--- pages/checkout_shipping.py
+++ pages/checkout_shipping.py
@@ -2,7 +2,8 @@ from selene import browser, be, have
 from selene.support.shared.jquery_style import s, ss
 
 def fill_shipping_address(user_email, first_name, last_name, street_address, city):
-    s('#customer-email-fieldset #customer-email').type(user_email)
+    # s("").with_(timeout=10).click()
+    s('#customer-email-fieldset #customer-email').with_(timeout=30).type(user_email)
     s('[name="firstname"]').type(first_name)
     s('[name="lastname"]').type(last_name)
     s('[name="street[0]"]').type(street_address)
