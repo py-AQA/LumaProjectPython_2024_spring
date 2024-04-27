7b373fd4bff448dc4a7915e190845335d9925db7
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2024-04-27 19:47:21 +0300

    [+] tests fix: 13 passed in 233.66s (0:03:53) (#9)

--- tests/auth/test_reset_password.py
+++ tests/auth/test_reset_password.py
@@ -3,5 +3,5 @@ from pages import forgot_password, message
 
 def test_reset_password():
     forgot_password.visit()
-    forgot_password.reset("sex@sex.com")
+    forgot_password.reset("jasonbrown1714146903@example.net")
     message.should_be("you will receive an email")
--- tests/auth/test_sign_in.py
+++ tests/auth/test_sign_in.py
@@ -3,8 +3,8 @@ from pages import sign_in, my_account, message
 
 def test_sign_in_with_good_credentials():
     sign_in.visit()
-    sign_in.login("jasonbrown1714146903@example.net", "VLK))5JiLA")
-    my_account.page_title("The account sign-in was incorrect")
+    sign_in.login("pamela341714226113@example.com", "@8j%Yltt(E")
+    my_account.page_title("My Account")
 
 
 def test_sign_in_with_bad_credentials():
