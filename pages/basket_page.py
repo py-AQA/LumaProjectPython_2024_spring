e35a342518f20fbc045d4b85746fe7177c2ca55e
Konstantin <161002599+Grom-Zadira85@users.noreply.github.com>
2024-04-30 00:09:37 +0300

    Basket page (#27)
    * mod  basket_page.py.py/pages
    mod test_basket_page.py
    
    * conflict
    
    * other files after conflict

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -3,14 +3,16 @@ from selene.support.shared.jquery_style import s, ss
 
 from pages.locators import ProductLocators as PL
 from pages.base_page import BasePage
+import allure
 
 
+@allure('https://trello.com/c/WVaLK93g')
 class BasketPage(BasePage):
-
     def add_to_cart_from_main_page(self):
         s(PL.ARGUS_All_WEATHER_TANK_SIZE).click()
         s(PL.ARGUS_All_WEATHER_TANK_COLOR).click()
         s(PL.ARGUS_All_WEATHER_TANK_ADD_TO_CARD).click()
+
 
     def check_link_present_in_the_window_mini_cart(self):
         s(PL.MINI_BASKET_WINDOW).click()
--- tests/test_basket_page.py
+++ tests/test_basket_page.py
@@ -3,6 +3,8 @@ import time
 from selene import browser
 
 from pages.basket_page import BasketPage
+import allure
+
 from pages.urls import main_page_link
 
 
@@ -12,6 +14,7 @@ def test_add_to_cart_from_main():
     page.add_to_cart_from_main_page()
 
 
+@allure('https://trello.com/c/WVaLK93g')
 def test_check_link_present_in_the_window_mini_cart():
     page = BasketPage(browser, main_page_link)
     page.open()
