7d9b79e25a5e3904f1cacc9c62088b9b1a5c2ca1
Mila Arh <rover321@mail.ru>
2024-04-30 11:21:34 +0400

    add test_women_tee.py redirection_from_women_tees_new (#28)

--- conftest.py
+++ conftest.py
@@ -22,7 +22,7 @@ def pytest_configure(config):
 def driver():
     options = Options()
     options.add_argument("--window-size=1920,1080")
-    # options.add_argument('--headless=new')
+    options.add_argument('--headless=new')
     options.add_argument("--lang=en")
     if os.environ.get("CI_RUN"):
         options.add_argument("--headless=new")
