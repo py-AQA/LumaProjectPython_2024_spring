cf793b500cbf601cca220fb7633d4a453bd2561e
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2024-05-01 01:08:31 +0300

    [+] test run 1

--- tests/auth/test_reset_password.py
+++ tests/auth/test_reset_password.py
@@ -4,4 +4,4 @@ from pages import forgot_password, message
 def test_reset_password():
     forgot_password.visit()
     forgot_password.reset("jasonbrown1714146903@example.net")
-    message.should_be("you will receive an email")
+    message.should_be("you wi111111111ll receive an email")
