fc81bce43a398eac6d10fa49a007bcec6a8a7132
Grom-Zadira85 <Trall18111985@gmail.com>
2024-04-30 22:54:48 +0300

    mod test_basket_page.py

--- tests/test_basket_page.py
+++ tests/test_basket_page.py
@@ -1,23 +1,35 @@
 import time
 
+import pytest
 from selene import browser
-
 from pages.basket_page import BasketPage
 import allure
-
 from pages.urls import main_page_link
 
 
+@allure.link('https://trello.com/c/WVaLK93g')
 def test_add_to_cart_from_main():
     page = BasketPage(browser, main_page_link)
     page.open()
     page.add_to_cart_from_main_page()
 
-
-@allure('https://trello.com/c/WVaLK93g')
-def test_check_link_present_in_the_window_mini_cart():
+@allure.link('https://trello.com/c/WVaLK93g')
+def test_color_and_clickability_of_View_and_edit_cart_link_in_the_mini_cart_tc_005_001_011():
     page = BasketPage(browser, main_page_link)
     page.open()
-    page.add_to_cart_from_main_page()
-    page.check_link_present_in_the_window_mini_cart()
+    page.check_color_and_clickability_of_View_and_edit_cart_link_in_the_mini_cart()
+
     time.sleep(5)
+    browser.quit()
+
+
+def test_Checking_hat_the_link_opens_the_cart_page_tc_005_001_012():
+    page = BasketPage(browser, main_page_link)
+    page.open()
+    page.Checking_hat_the_link_opens_the_cart_page()
+
+@pytest.mark.skip
+def test_color_button():
+    page = BasketPage(browser, main_page_link)
+    page.open()
+    page.check_color_button()
