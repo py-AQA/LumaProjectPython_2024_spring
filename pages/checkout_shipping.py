0914db2bef4cfe5846f16f883f4f7e77cb6c655b
Mila Arh <rover321@mail.ru>
2024-05-03 02:36:41 +0400

    add test_checkout_from_mini_cart

--- pages/checkout_shipping.py
+++ pages/checkout_shipping.py
@@ -1,14 +1,43 @@
 from selene import browser, be, have
 from selene.support.shared.jquery_style import s, ss
 
+from pages.user import Person
+from selene.support import by
 
-def fill_shipping_address(user_email, first_name, last_name, street_address, city):
+# def fill_shipping_address(user):
+#     # s("").with_(timeout=10).click()
+#     s('#customer-email-fieldset #customer-email').with_(timeout=30).type(user['user_email'])
+#     s('[name="firstname"]').type(user.get("first_name"))
+#     s('[name="lastname"]').type(user['last_name'])
+#     s('[name="street[0]"]').type(user['street_address'])
+#     s('[name="city"]').type(user['city'])
+#     s('[name="region_id"]').press('Colorado')
+#     s('[name="postcode"]').type(user['postcode'])
+#     s('[name="country_id"]').press('Colorado')
+#     s('[name="country_id"]').click()
+#     s('[value="AI"]').click()
+#     s('[name="telephone"]').type(user['phone_number'])
+#     s('#checkout-shipping-method-load input').click()
+#     s('.continue').click()
+
+def fill_shipping_address_class():
+    person1 = Person()
     # s("").with_(timeout=10).click()
-    s('#customer-email-fieldset #customer-email').with_(timeout=30).type(user_email)
-    s('[name="firstname"]').type(first_name)
-    s('[name="lastname"]').type(last_name)
-    s('[name="street[0]"]').type(street_address)
-    s('[name="city"]').type(city)
+    s('#customer-email-fieldset #customer-email').with_(timeout=30).type(person1.user_email)
+    s('[name="firstname"]').type(person1.first_name)
+    s('[name="lastname"]').type(person1.last_name)
+    s('[name="street[0]"]').type(person1.street_address)
+    s('[name="city"]').type(person1.city)
+    s('[name="region_id"]').press('Colorado')
+    s('[name="postcode"]').type(person1.postcode)
+    s('[name="country_id"]').press('Colorado')
+    s('[name="country_id"]').click()
+    s('[value="AI"]').click()
+    s('[name="telephone"]').type(person1.phone_number)
+    s('#checkout-shipping-method-load input').click()
+    s('.continue').click()
+    # s('#billing-address-same-as-shipping-checkmo').with_(timeout=30).click()
+    s('.primary.checkout').click()
 
     # Находим выпадающий список по селектору
     # select_element = browser.s('[name="region_id"]')
@@ -16,7 +45,7 @@ def fill_shipping_address(user_email, first_name, last_name, street_address, cit
     # Выбираем опцию по тексту
     # select_element.select('Option 1')
 
-    # s('[name="firstname"]').type(first_name)
-    # s('name="lastname"').type(last_name)
+    # type(first_name)
+    #
     # s('name="street[0]"').type(street_address)
     # s('name="street[0]"').type(street_address)
--- pages/mini_cart.py
+++ pages/mini_cart.py
@@ -5,6 +5,6 @@ from selene.support.shared.jquery_style import s, ss
 
 
 def checkout():
-    # time.sleep(3)
+    time.sleep(3)
     s('.viewcart').should(be.clickable)
     s('#top-cart-btn-checkout').should(be.clickable).click()
