39f538a4c5a5016b64b91710b3095f09e335f3ef
Konstantin <161002599+Grom-Zadira85@users.noreply.github.com>
2024-05-03 13:56:53 +0300

    Refactor in  class BasketPage
    * Replacing the steps of adding a product to the cart with self in all functions
    
    * Attempt to move def open to fixture

--- tests/test_basket_page.py
+++ tests/test_basket_page.py
@@ -1,10 +1,19 @@
-import time
-
 import pytest
 from selene import browser
 from pages.basket_page import BasketPage
 import allure
 from pages.urls import main_page_link
+
+
+# тестов  мало поэтому нет необходимости выносить это в фикстуру
+# @pytest.fixture(scope="function")
+# def open_main_page():
+#     page = BasketPage(browser, main_page_link)
+#     page.open()
+#     return page
+
+# def test_add_to_cart_from_main(open_main_page):
+#     open_main_page.add_to_cart_from_main_page()
 
 
 @allure.link('https://trello.com/c/WVaLK93g')
@@ -13,23 +22,23 @@ def test_add_to_cart_from_main():
     page.open()
     page.add_to_cart_from_main_page()
 
+
 @allure.link('https://trello.com/c/WVaLK93g')
-def test_color_and_clickability_of_View_and_edit_cart_link_in_the_mini_cart_tc_005_001_011():
+def test_color_and_clickability_of_view_and_edit_cart_link_in_the_mini_cart_tc_005_001_011():
     page = BasketPage(browser, main_page_link)
     page.open()
-    page.check_color_and_clickability_of_View_and_edit_cart_link_in_the_mini_cart()
-
-    time.sleep(5)
-    browser.quit()
+    page.check_color_and_clickability_of_view_and_edit_cart_link_in_the_mini_cart()
+    # time.sleep(5)
 
 
-def test_Checking_hat_the_link_opens_the_cart_page_tc_005_001_012():
+@allure.link('https://trello.com/c/w8Of4GYe')
+def test_checking_the_link_opens_the_cart_page_tc_005_001_012():
     page = BasketPage(browser, main_page_link)
     page.open()
-    page.Checking_hat_the_link_opens_the_cart_page()
+    page.checking_the_link_opens_the_cart_page()
 
-@pytest.mark.skip
-def test_color_button():
-    page = BasketPage(browser, main_page_link)
-    page.open()
-    page.check_color_button()
+# @pytest.mark.skip #  пробный искали цвет
+# def test_color_button():
+#     page = BasketPage(browser, main_page_link)
+#     page.open()
+#     page.check_color_button()
