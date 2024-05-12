e73bbfc317aa32aa8762053c236d1269951831dd
Grom-Zadira85 <Trall18111985@gmail.com>
2024-05-12 16:46:32 +0300

    added functions necessary to fulfill the mini-cart testing preconditions

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,7 +1,9 @@
 from selene.support.shared.jquery_style import s, ss
-
+from selene import  be
 from pages.base_page import BasePage
-from pages.locators import NavigatorLocators as NL
+# from pages.basket_page import BasketPage
+from pages.locators import NavigatorLocators as NL, ProductLocators as PL
+from pages.urls import main_page_link
 
 
 class HomePage(BasePage):
@@ -16,3 +18,11 @@ class HomePage(BasePage):
 
     def navigate_to(self, locator):
         s(locator).click()
+
+    def add_to_cart_from_main_page(self):
+        s(PL.ARGUS_All_WEATHER_TANK_SIZE).click()
+        s(PL.ARGUS_All_WEATHER_TANK_COLOR).click()
+        s(PL.ARGUS_All_WEATHER_TANK_ADD_TO_CARD).click()
+
+    def go_to_mini_cart(self):
+        s(PL.MINI_BASKET_WINDOW).should(be.clickable).click()
